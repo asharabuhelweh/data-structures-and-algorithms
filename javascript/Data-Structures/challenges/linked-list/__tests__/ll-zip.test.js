3bb07abc7fefdaeb9a87e12f4535158a1ab6a06f
asharabuhelwh <ashar.mohamad@yahoo.com>
2021-06-04 23:23:21 +0300

    edit the zip list function

--- javascript/Data-Structures/challenges/linked-list/__tests__/ll-zip.test.js
+++ javascript/Data-Structures/challenges/linked-list/__tests__/ll-zip.test.js
@@ -5,76 +5,79 @@ const ZipLL = require('../ll-zip.js');
 const { expect } = require('@jest/globals');
 
 
-describe('zip Linked Lists',()=>{
-   it ('should zip tow linked lists into one when the second longer than the first',()=>{
+describe('zip Linked Lists', () => {
+  it('should zip tow linked lists into one when the second longer than the first', () => {
 
-   //arrange
-   let LL1 = new LinkedList ();
-   let LL2 = new LinkedList ();
-   LL1.insert (1);
-   LL1.insert (3);
-   LL1.append(5);
-   LL2.insert ('a');
-   LL2.insert ('b');
-   LL2.insert ('c');
-   LL1.append('d');
-  
-   //act
-   let zip = ZipLL (LL1 , LL2);
-   //assert
-   expect (zip.head.value).toBe (3);
-   expect (zip.head.next.value).toBe ('c');
-   expect (zip.head.next.next.value).toBe (1);
-   expect (zip.head.next.next.next.value).toBe ('b');
-   expect (zip.head.next.next.next.next.value).toBe (5);
-   expect (zip.head.next.next.next.next.next.value).toBe ('a');
-   expect (zip.head.next.next.next.next.next.next.value).toBe ('d');
+    //arrange
+    let LL1 = new LinkedList();
+    let LL2 = new LinkedList();
+    LL1.insert(1);
+    LL1.insert(3);
+    LL1.append(5);
+    LL2.insert('a');
+    LL2.insert('b');
+    LL2.insert('c');
+    LL1.append('d');
 
-
- });
+    //act
+    let zip = ZipLL(LL1, LL2);
+    //assert
+    expect(zip.head.value).toBe(3);
+    expect(zip.head.next.value).toBe('c');
+    expect(zip.head.next.next.value).toBe(1);
+    expect(zip.head.next.next.next.value).toBe('b');
+    expect(zip.head.next.next.next.next.value).toBe(5);
+    expect(zip.head.next.next.next.next.next.value).toBe('a');
+    expect(zip.head.next.next.next.next.next.next.value).toBe('d');
 
 
- it ('should zip tow linked lists into one when the first is longer',()=>{
-
-   //arrange
-   let LL1 = new LinkedList ();
-   let LL2 = new LinkedList ();
-   LL1.insert (100);
-   LL1.insert (200);
-   LL1.insert (300);
-   LL1.append(400);
-
-   LL2.insert ('a');
-   LL2.insert ('b');
-   
-   //act
-   let zip = ZipLL (LL1 , LL2);
-   //assert
-   expect (zip.head.value).toBe (300);
-   expect (zip.head.next.value).toBe ('b');
-   expect (zip.head.next.next.value).toBe (200);
-   expect (zip.head.next.next.next.value).toBe ('a');
-   expect (zip.head.next.next.next.next.value).toBe (100);
-   expect (zip.head.next.next.next.next.next.value).toBe (400);
+  });
 
 
+  it('should zip tow linked lists into one when the first is longer', () => {
+
+    //arrange
+    let LL1 = new LinkedList();
+    let LL2 = new LinkedList();
+    LL1.insert(100);
+    LL1.insert(200);
+    LL1.insert(300);
+    LL1.append(400);
+
+    LL2.insert('a');
+    LL2.insert('b');
+
+    //act
+    let zip = ZipLL(LL1, LL2);
+    //assert
+    expect(zip.head.value).toBe(300);
+    expect(zip.head.next.value).toBe('b');
+    expect(zip.head.next.next.value).toBe(200);
+    expect(zip.head.next.next.next.value).toBe('a');
+    expect(zip.head.next.next.next.next.value).toBe(100);
+    expect(zip.head.next.next.next.next.next.value).toBe(400);
 
 
- });
- 
- 
 
-it("Edge case ,pass array instead of linked list", () => {
 
-   let zipLL = ZipLL([5, 10], "a");
+  });
 
-   expect(zipLL.head).toBeNull()
 
+
+
+
+  it('Edge case , passing no parameter or undefined value', () => {
+
+    let zipLL =ZipLL();
+    
+
+    expect(zipLL).toBe('please insert valid values!');
+
+
+
+  });
 
 });
-
-
-})
 
 
 
--- javascript/Data-Structures/challenges/linked-list/ll-zip.js
+++ javascript/Data-Structures/challenges/linked-list/ll-zip.js
@@ -3,26 +3,63 @@
 const LinkedList = require('./linked-list.js');
 const Node = require('./node.js');
 
+// function zipLL(firstList, secondList) {
+
+//   let zipLL = new LinkedList;
+
+//   let firstValue = firstList.head; //first value in the first LL and it will be the first in zipLL
+//   let secondValue = secondList.head; // first value in the second LL and it will be the second zipLL
+
+//   while (firstValue || secondValue) {
+//     if (firstValue) {
+//       zipLL.append(firstValue.value);
+//       firstValue = firstValue.next;
+//     }
+//     if (secondValue) {
+//       zipLL.append(secondValue.value);
+//       secondValue = secondValue.next;
+//     }
+
+//   }
+
+//   return zipLL;
+// }
 function zipLL(firstList, secondList) {
 
-    let zipLL = new LinkedList;
+  if (!firstList && !secondList && firstList === undefined && secondList === undefined) {
+    return ('please insert valid values!');
+  }
 
-    let firstValue = firstList.head; //first value in the first LL and it will be the first in zipLL
-    let secondValue = secondList.head; // first value in the second LL and it will be the second zipLL
+  if (firstList.head === null && !secondList.head === null) {
+    return secondList.head;
+  }
 
-    while (firstValue || secondValue) {  
-        if (firstValue) {
-            zipLL.append(firstValue.value); 
-            firstValue = firstValue.next;
-        }
-        if (secondValue) {
-            zipLL.append(secondValue.value);
-            secondValue = secondValue.next;
-        }
+  if (secondList.head === null && !firstList.head === null) {
+    return firstList.head;
+  }
 
-    }
+  let firstValue = firstList.head;
+  let secondValue = secondList.head;
+  while (firstValue.next && secondValue.next) {
+    firstList.insertAfter(firstValue.value, secondValue.value);
+    secondValue = secondValue.next;
+    firstValue = firstValue.next.next;
+  }
 
-    return zipLL
+  while (secondValue.next) {
+    firstList.append(secondValue.value);
+    secondValue = secondValue.next;
+    firstValue = firstValue.next;
+  }
+
+  if (!secondValue.next && !firstValue.next) {
+    firstList.append(secondValue.value);
+  } else {
+    firstList.insertAfter(firstValue.value, secondValue.value);
+  }
+  return firstList;
+
+
 }
 
 module.exports = zipLL;
